9fbba4b5602aa91fcb536b4f5440018e81c556f9
Muhammad Khuzaima Siddiqui <mksprogramify@gmail.com>
2025-01-06 01:57:01 +0500

    Delete 02(ii)_Conditional_Statement ( Ternary Operator ) directory

--- 02(ii)_Conditional_Statement ( Ternary Operator )/3_PassFail.cxx
+++
@@ -1,9 +0,0 @@
-# include<iostream>
-using namespace std;
-int main(){
-    float marks;
-    cout<<"Enter Marks : ";
-    cin>>marks;
-    (marks > 33) ? cout<<"Passed!" : cout<<"Fail!"; 
-    return 0;
-}
--- 02(ii)_Conditional_Statement ( Ternary Operator )/6_PredictOutput.cxx
+++
@@ -1,8 +0,0 @@
-# include<iostream>
-using namespace std;
-int main(){
-    int test = 0;
-    float f = 3.111;
-    cout<<( test > 0 ? f : 0 ) << endl;  
-    return 0;
-}
